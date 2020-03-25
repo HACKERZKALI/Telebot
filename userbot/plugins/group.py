8b146ea6da27cbeb8697b3ca3c994d6e416a41c4
HACKERZKALI <62069925+HACKERZKALI@users.noreply.github.com>
2020-03-25 19:42:06 +0530

    Update group.py

--- userbot/plugins/group.py
+++ userbot/plugins/group.py
@@ -6,4 +6,4 @@ async def join(e):
 
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
 
-        await e.edit("This is my community.\n\n[Channel](http://t.me/giveaways_24hrs)\n\n[Chat Group](http://t.me/giveaways24hrsdiscuss)\n\n[UserBot Tutorial](https://t.me/TeleBotHelp)")
+        await e.edit("This is my community.\n\n[Netflix Commix](http://t.me/netflix_coomix)\n\n[Eonarmy](http://t.me/eonarmy)\n\n[Eonarmy Bins](https://t.me/eonarmy_bins)\n\n[Eonarmy Network](http://t.me/eonarmy_network)")
