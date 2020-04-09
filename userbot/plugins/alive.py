1d4ee661318fba70af42a2b5b6a28b2262efea09
HACKERZKALI <62069925+HACKERZKALI@users.noreply.github.com>
2020-04-09 10:58:49 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,14 +6,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@EYEGANG_XD @NETFLIX_NETHAX "
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`JINDA HU TODAYSHACKER SIR`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nFork by:` @TODAYSHACKER\n"
-                     "`My creater is:` [TODAYSHACKER](tg://user?id=1116223246)\n"
+    await alive.edit("**`JINDA HU EYE GANG MEMBERS`**\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nFork by:` @EYEGANG_XD\n"
+                     "`My creater is:` [EYE GANG XD](tg://user?id=1036393307)\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, My master!\n`"
-                     f"`My sir`: @TODAYSHACKER \n"
-                     "[JOIN MY SIR CHANNEL](https://t.me/netflix_commix)")
+                     f"`My sir`: {DEFAULTUSER} \n"
+                     "[JOIN OUR CHANNEL](https://t.me/netflix_nethax)")
